7c658f266251ce19f758173d7302a562a34fbdaf
Pedro Machado <pedromgebauer@gmail.com>
2026-06-25 13:07:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+       Console.Write("valorPresente: ");
+       double capitalInicial = double.Parse(Console.ReadLine()); 
+
+       Console.Write("Taxa: ");
+       decimal percentual = decimal.Parse(Console.ReadLine());
+
+       Console.Write("Período (Meses ou Ano): ");
+       int tempo = int.Parse(Console.ReadLine());
+
+       decimal taxa = percentual / 100;
+
+       double montanteFinal = calcularJuros(capitalInicial, taxa, tempo);
+
+       Console.WriteLine($"Valor Final: {montanteFinal:F2}");
+
+
+    }
+
+    static double calcularJuros(double valorPresente, decimal taxaJuros, int periodo)
+    {
+        return valorPresente * Math.Pow(1 + (double)taxaJuros, periodo);
+    }
+}
